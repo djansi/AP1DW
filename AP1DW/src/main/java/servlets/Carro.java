e1cb5787a21cfe2694b1c4c71059e231e8c65ca6
Jansen <133006428+djansi@users.noreply.github.com>
2023-09-20 17:43:46 -0300

    1

---
+++ AP1DW/src/main/java/servlets/Carro.java
@@ -0,0 +1,31 @@
+package servlets;
+
+public class Carro {
+	
+	public String modelo;
+	public String marca; 
+	public Integer anoFabrica;
+	
+	public String getModelo() {
+		return modelo;
+	}
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+	public String getMarca() {
+		return marca;
+	}
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+	public Integer getAnoFabrica() {
+		return anoFabrica;
+	}
+	public void setAnoFabricadao(Integer anoFabrica) {
+		this.anoFabric = anoFabrica;
+	}
+	
+	
+	
+
+}
